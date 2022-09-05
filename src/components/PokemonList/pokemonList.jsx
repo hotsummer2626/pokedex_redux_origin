5895160a7a07381b36e055d5e316f85f218ee86e
SUNNY <hotsummer2626@gmail.com>
2022-09-05 16:25:14 +1000

    add loading component

--- src/components/PokemonList/pokemonList.jsx
+++ src/components/PokemonList/pokemonList.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import styles from "./pokemonList.module.scss";
 import Card from "./Card/card";
 import { connect } from "react-redux";
+import Loading from "./Loading";
 
 const PokemonList = ({ pokemonList }) => {
   return (
     <ul className={styles.container}>
-      {pokemonList.map((pokemon) => (
-        <Card key={pokemon.id}  pokemon={pokemon} />
-      ))}
+      {pokemonList.length !== 0 ? (
+        pokemonList.map((pokemon) => (
+          <Card key={pokemon.id} pokemon={pokemon} />
+        ))
+      ) : (
+        <Loading />
+      )}
     </ul>
   );
 };
